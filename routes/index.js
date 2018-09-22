02d974051af25977fa36435368335e7762366ddd
Markus Sorg <markus.sorg@gmail.com>
2018-09-22 23:13:25 +0200

    Add node version

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,8 @@ nconf.file({ file: process.argv[2] || './dev-config.json' });
 router.get('/', function(req, res, next) {
   res.render('index', {
     title: nconf.get('title'),
-    message: nconf.get('message')
+    message: nconf.get('message'),
+    nodeVersion: process.version
   });
 });
 
